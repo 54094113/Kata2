4f4a90a153feca3eab5aaf487b1471098de00ec4
usuario <usuario@10.22.148.56>
2017-10-06 10:04:46 +0100

    Cuarta versión de Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,12 +5,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] data = {1,1,1,0,3,4,3,0,0,1,1,1,1,3,2};
+        String[] data = {"hola","buenas tardes","¿Qué frase?"};
         Histogram histo = new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
+        Map<String,Integer> histogr = histo.getHistogram();
                 
-        for(int key : histogr.keySet()){
-            System.out.println("El número " + key + " se repite " 
+        for(String key : histogr.keySet()){
+            System.out.println("'" + key + "'" + " aparece " 
                              + histogr.get(key) + " vez/veces.");
         }
              
