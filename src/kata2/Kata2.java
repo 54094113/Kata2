c663490305d085676382b41806237e3034f0899b
usuario <usuario@10.22.148.56>
2017-10-06 09:27:51 +0100

    Primera versión de Kata2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,17 +9,14 @@ public class Kata2 {
         int[] data = {1,1,1,0,3,4,3,0,0,1,1,1,1,3,2};
         Map<Integer,Integer> histogram = new HashMap<>();
         
-        for (int i=0; i<data.length; i++){
-            if (histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            } else {
-                histogram.put(data[i], 1);
-            }
+        for(int key : data){
+            histogram.put(key, histogram.containsKey(key) ?
+                          histogram.get(key)+1 : 1);
         }
         
         for(int key : histogram.keySet()){
             System.out.println("El número " + key + " se repite " 
-                             + histogram.get(key) + " veces.");
+                             + histogram.get(key) + " vez/veces.");
         }
              
     }
